360f38bd1078fd624bc3e4ee0d74ece7a9ce341e
MisteryDevs <officialbotdev@gmail.com>
2025-05-24 09:36:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/papahuu/anonymousxsana",
+    "https://github.com/MisteryDevs/madharchod",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
